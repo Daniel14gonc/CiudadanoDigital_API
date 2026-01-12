bacf61374a73791137cbeab2bbe0bf376005cf8a
Daniel Gonzalez <danielgo.carrillo@gmail.com>
2026-01-12 17:36:10 -0600

    fix: usage of s3 config terraform backend

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,12 +1,6 @@
 # Terraform config
 terraform {
-  backend "s3" {
-    bucket         = var.s3_terraform_state_bucket
-    key            = "prod/terraform.tfstate"
-    region         = var.aws_region
-    dynamodb_table = var.dynamo_db_terraform_lock_table
-    encrypt        = true
-  }
+  backend "s3" {}
 
   required_providers {
     aws = {
